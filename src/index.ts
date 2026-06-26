2176f5fd589aad85e9de712da65e75d06877a7d8
shailp653-debug <shailp653@gmail.com>
2026-06-26 16:23:21 +0530

    Add a route to check if the backend is running

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,9 @@ app.use((err: Error, _req: express.Request, res: express.Response, _next: expres
     message: 'An unexpected error occurred on the server.'
   });
 });
-
+app.get("/", (_req, res) => {
+  res.send("Backend is running");
+});
 // Start Server
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
